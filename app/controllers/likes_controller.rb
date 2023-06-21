bf8da797521d98aee03c541c83b033ab14c7bc97
komanosuke <toshihiko.komai616@gmail.com>
2023-06-21 18:31:03 +0900

    update

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,10 +3,27 @@ class LikesController < ApplicationController
         @post = Post.find(params[:post_id])
         like = current_user.likes.build(post_id: params[:post_id])
         @posts = Post.all
+        @params_url = params[:url]
         respond_to do |format|
             if like.save
-                format.html { redirect_to 'posts/index' }
-                format.js
+                if params[:url] == 'index'
+                    format.html { redirect_to 'posts/index' }
+                    format.js
+                elsif params[:url] == 'show'
+                    format.html { redirect_to 'posts/' + params[:post_id] }
+                    format.js
+                elsif params[:url] == 'posted'
+                    @post_cat = params[:post_cat]
+                    if @post_cat == 'posted'
+                        @posts = User.find(params[:user_id]).posts
+                    elsif @post_cat == 'liked'
+                        @posts = User.find(params[:user_id]).like_posts
+                    elsif @post_cat == 'commented'
+                        @posts = User.find(params[:user_id]).comment_posts
+                    end
+                    format.html { redirect_to 'posted/' + params[:user_id]}
+                    format.js
+                end
             else
                 flash[:error] = like.errors.full_messages.join(', ')
                 format.html { redirect_to posts_path }
@@ -18,10 +35,27 @@ class LikesController < ApplicationController
         @post = Post.find(params[:post_id])
         like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
         @posts = Post.all
+        @params_url = params[:url]
         respond_to do |format|
             if like.destroy
-                format.html { redirect_to 'posts/index' }
-                format.js
+                if params[:url] == 'index'
+                    format.html { redirect_to 'posts/index' }
+                    format.js
+                elsif params[:url] == 'show'
+                    format.html { redirect_to 'posts/' + params[:post_id] }
+                    format.js
+                elsif params[:url] == 'posted'
+                    @post_cat = params[:post_cat]
+                    if @post_cat == 'posted'
+                        @posts = User.find(params[:user_id]).posts
+                    elsif @post_cat == 'liked'
+                        @posts = User.find(params[:user_id]).like_posts
+                    elsif @post_cat == 'commented'
+                        @posts = User.find(params[:user_id]).comment_posts
+                    end
+                    format.html { redirect_to 'posted/' + params[:user_id]}
+                    format.js
+                end
             else
                 format.html { redirect_to @post, alert: "Failed to unlike post." }
             end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -10,15 +10,32 @@ class RelationshipsController < ApplicationController
                 @relationships = Relationship.where(follower_id: current_user.id).order(created_at: :desc)
                 format.html { redirect_to relationship_params[:url] }
                 format.js
-              elsif relationship_params[:url].include?('posts')
+              elsif relationship_params[:url].include?('follower')
+                @relationships = Relationship.where(followed_id: current_user.id).order(created_at: :desc)
+                format.html { redirect_to relationship_params[:url] }
+                format.js
+              elsif relationship_params[:url] == '/posts'
                 @posts = Post.all
                 format.html { redirect_to relationship_params[:url] }
                 format.js
-              elsif relationship_params[:url].include?('posted')
+              elsif relationship_params[:url].include?('posts')
                 id = relationship_params[:url].split("/").last.to_i
-                @user = User.find(id)
-                @posts = @user.posts
+                @post = Post.find(id)
                 format.html { redirect_to relationship_params[:url] }
+                format.js
+              elsif relationship_params[:url].include?('posted')
+                url = relationship_params[:url].split('/')[0..-2].join('/')
+                id = url.split('/')[2].to_i
+                @post_cat = relationship_params[:url].split('/').last
+                @user = User.find(id)
+                if @post_cat == 'posted'
+                  @posts = @user.posts
+                elsif @post_cat == 'liked'
+                  @posts = @user.like_posts
+                elsif @post_cat == 'commented'
+                  @posts = @user.comment_posts
+                end
+                format.html { redirect_to url }
                 format.js
               end
             end
@@ -39,15 +56,32 @@ class RelationshipsController < ApplicationController
               @relationships = Relationship.where(follower_id: current_user.id).order(created_at: :desc)
               format.html { redirect_to params[:url] }
               format.js
-            elsif params[:url].include?('posts')
+            elsif params[:url].include?('follower')
+              @relationships = Relationship.where(followed_id: current_user.id).order(created_at: :desc)
+              format.html { redirect_to params[:url] }
+              format.js
+            elsif params[:url] == '/posts'
               @posts = Post.all
               format.html { redirect_to params[:url] }
               format.js
-            elsif params[:url].include?('posted')
+            elsif params[:url].include?('posts')
               id = params[:url].split("/").last.to_i
-              @user = User.find(id)
-              @posts = @user.posts
+              @post = Post.find(id)
               format.html { redirect_to params[:url] }
+              format.js
+            elsif params[:url].include?('posted')
+              url = params[:url].split('/')[0..-2].join('/')
+              id = url.split('/')[2].to_i
+              @post_cat = params[:url].split('/').last
+              @user = User.find(id)
+              if @post_cat == 'posted'
+                @posts = @user.posts
+              elsif @post_cat == 'liked'
+                @posts = @user.like_posts
+              elsif @post_cat == 'commented'
+                @posts = @user.comment_posts
+              end
+              format.html { redirect_to url }
               format.js
             end
           end
